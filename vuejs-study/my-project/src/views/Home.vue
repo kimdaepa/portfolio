f2a982f0159bbf31c4101196c84c7a37bd8dab3c
kimdaepa <kimdaepa@gmail.com>
2019-12-02 15:26:35 +0900

    tab 다른 스타일을 줘보자

--- vuejs-study/my-project/src/views/Home.vue
+++ vuejs-study/my-project/src/views/Home.vue
@@ -6,6 +6,7 @@
       <BaseBannerButton :class="$style.button">Left Button</BaseBannerButton>
       <BaseBannerButton :class="$style.button" color-type="white_outline">Right Button</BaseBannerButton>
     </BaseBanner>
+    <BaseTab :lists="tabborder" type="tab_border"></BaseTab>
   </BaseLayout>
 </template>
 
@@ -14,13 +15,37 @@
 import BaseLayout from '@/components/base-layout.vue'
 import BaseBanner from '@/components/base-banner.vue'
 import BaseBannerButton from '@/components/base-banner-button.vue'
+import BaseTab from '@/components/base-tab.vue'
+
+// tab border 필요한 데이터 값 배열
+const tabborder = [
+  {
+    text: 'tab1',
+    href: '/tab1'
+  },
+  {
+    text: 'tab2',
+    href: '/tab2'
+  },
+  {
+    text: 'tab3',
+    href: '/tab3'
+  }
+]
 
 export default {
   name: 'home',
   components: {
     BaseLayout,
     BaseBanner,
-    BaseBannerButton
+    BaseBannerButton,
+    BaseTab
+  },
+  // gnb 배열을 사용하겠다고 선언하자
+  data () {
+    return {
+      tabborder
+    }
   }
 }
 </script>
